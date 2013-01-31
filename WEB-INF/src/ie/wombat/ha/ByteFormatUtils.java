c6fc62d5c659a18914b233f18081b38c6ebb41d3
Joe Desbonnet <jdesbonnet@gmail.com>
2013-01-31 01:43:08 +0000

    Add documentation.

--- WEB-INF/src/ie/wombat/ha/ByteFormatUtils.java
+++ WEB-INF/src/ie/wombat/ha/ByteFormatUtils.java
@@ -2,11 +2,17 @@ package ie.wombat.ha;
 
 public class ByteFormatUtils {
 
+	/**
+	 * Convert a string of hex digits into a byte[] array. Spaces 
+	 * in string are ignored, but otherwise must be 0-9, a-f, A-F.
+	 * 
+	 * @param byteString 
+	 * @return Byte array of length = number of hex digits / 2.
+	 */
 	public static byte[] stringToByteArray(String byteString) {
 		
 		// Remove white space
 		byteString = byteString.replaceAll("\\s", "");
-		
 		
 		int n = byteString.length()/2;
 		byte[] ret = new byte[n];
@@ -15,13 +21,25 @@ public class ByteFormatUtils {
 		}
 		return ret;
 	}
-	public static String byteArrayToString(byte[] bytes) {
-		//if (bytes == null) {
-		//	return "(null)";
-		//}
-		
+	
+	/**
+	 * Convert a byte[] to a string of hex digits. 
+	 * 
+	 * @param bytes
+	 * @return String of hex digits where length = size of byte array * 2
+	 */
+	public static String byteArrayToString(byte[] bytes) {		
 		return byteArrayToString(bytes,0,bytes.length);
 	}
+	
+	/**
+	 * Convert a byte[] to a string of hex digits specifying a offset into 
+	 * the array and a length. If length exceeds the size of the byte array
+	 * then length is reduced so that it corresponds to the end of the array.
+	 * 
+	 * @param bytes
+	 * @return String of hex digits
+	 */
 	public static String byteArrayToString(byte[] bytes, int offset, int len) {
 		if (bytes == null) {
 			return "(null)";
@@ -41,6 +59,13 @@ public class ByteFormatUtils {
 		buf.setLength(buf.length()-1);
 		return buf.toString();
 	}
+	
+	/**
+	 * Return 2 character hex representation of the parameter. 
+	 * 
+	 * @param b
+	 * @return
+	 */
 	public static String formatHexByte (int b) {
 		b &= 0xff;
 		if (b < 16) {
@@ -48,6 +73,13 @@ public class ByteFormatUtils {
 		}
 		return Integer.toHexString(b);
 	}
+	
+	/**
+	 * Return 8 character binary representation of the parameter. 
+	 * 
+	 * @param b
+	 * @return
+	 */
 	public static String formatBinaryByte (int b) {
 		StringBuffer buf = new StringBuffer();
 		for (int i = 0; i < 8; i++) {
